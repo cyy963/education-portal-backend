2cfa9fbeefeb93b8e2ab283fd9db0be478f172f2
justine padilla <158981238+justinedp@users.noreply.github.com>
2024-04-18 18:45:40 +1200

    committing changes

--- server.js
+++ server.js
@@ -1,15 +1,20 @@
 // ====== Packages and imports ====== //
 const express = require("express");
+const cors = require("cors");
 const app = express();
 require("dotenv").config();
 // get the client
 const mysql = require("mysql2");
 
+// ========== Middleware ============= //
+app.use(cors());
+app.use(express.json());
+
 // Create the connection to database
 const pool = mysql.createConnection({
   host: process.env.MYSQL_HOST,
   user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
+  password: process.env.MYSQL_PASS,
   database: process.env.MYSQL_DATABASE,
   waitForConnections: true, // people can wait
   connectionLimit: 10, // 10 slots at one time
@@ -20,6 +25,46 @@ const pool = mysql.createConnection({
 // Initial setup in Postman
 app.get("/", (req, res) => {
   res.send("Hello, World!");
+});
+
+// =========== GET for help requests =========== //
+app.get("/api/help-requests", (req, res) => {
+  console.log("Endpoint reached");
+
+  const query = `SELECT request_id, date_created, name, profile_pic, done FROM help_request INNER JOIN student ON help_request.student_id = student.student_id WHERE done = 0;`;
+
+  pool.execute(query, (err, result) => {
+    if (err) {
+      console.log("Database error:", err);
+      return res.status(500).json({
+        errorMessage:
+          "An error occurred while fetching data from the database.",
+      });
+    }
+
+    console.log(result);
+    res.send(result);
+  });
+});
+
+// =========== GET for teacher profile viewer =========== //
+app.get("/api/teacher-profile-viewer", (req, res) => {
+  console.log("Endpoint reached");
+
+  const query = `SELECT teacher_id, name, email, school, profile_pic, date_of_birth, contact_number FROM teacher;`;
+
+  pool.execute(query, (err, result) => {
+    if (err) {
+      console.log("Database error:", err);
+      return res.status(500).json({
+        errorMessage:
+          "An error occurred while fetching data from the database.",
+      });
+    }
+
+    console.log(result);
+    res.send(result);
+  });
 });
 
 // ============== PORT ============== //
