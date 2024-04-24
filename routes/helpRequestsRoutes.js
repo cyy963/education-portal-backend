bdd0a7b38c9ee3cdc01b0c68170dbcc901f71d6b
justine padilla <158981238+justinedp@users.noreply.github.com>
2024-04-24 20:25:21 +1200

    added another POST query for help requests

--- routes/helpRequestsRoutes.js
+++ routes/helpRequestsRoutes.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const pool = require("../model/db.js");
-
-
+ 
+ 
 // =========== GET for help requests =========== //
 router.get("/help-requests", (req, res) => {
     console.log("Endpoint reached");
-  
-    const query = ` SELECT request_id, student_name, date_created, profile_pic, done FROM help_request INNER JOIN student ON help_request.student_id = student.student_id WHERE done = 0;`;
-  
+ 
+    const query = ` SELECT help_request.student_id, request_id, student_name, date_created, profile_pic, done FROM help_request INNER JOIN student ON help_request.student_id = student.student_id WHERE done = 0;`;
+ 
     pool.execute(query, (err, result) => {
       if (err) {
         console.log("Database error:", err);
@@ -17,11 +17,13 @@ router.get("/help-requests", (req, res) => {
             "An error occurred while fetching data from the database.",
         });
       }
-  
+ 
       console.log(result);
       res.send(result);
     });
   });
+ 
+  module.exports = router
 
 /////// POST for help requests
 
