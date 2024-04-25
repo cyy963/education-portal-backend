1d82407b32f17e564b1a15e640982b724722492f
cyy963 <cyrusw89@gmail.com>
2024-04-26 11:31:05 +1200

    comment update

--- routes/studentProfilesRoutes.js
+++ routes/studentProfilesRoutes.js
@@ -4,9 +4,9 @@ const pool = require("../model/db.js");
 
 //============ teacher-dashboard/student-profiles ============
 router.get('/students',(req,res)=>{
-    pool.query(`SELECT student_name, profile_pic, student_id FROM \`missionr_2402-L4FT13-team3\`.student;`, (err,result)=>{
+    pool.query(`SELECT student_name, student_id, profile_pic FROM student;`, (err,result)=>{
         if (err){
-            console.log('database error:', err);
+            console.log('database error:', err);    
             return res.status(500).json({errorMessage:'an error while fetching the database.'})
         } else{
             res.send(result);
